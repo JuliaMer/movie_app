c17e1d1d2988a8f21f49c2a288bdf1e25718ecfb
Julia Merk <julia.merk@makandra.de>
2018-04-23 11:50:25 +0200

    added edit view

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -2,7 +2,7 @@ class ActorsController < ApplicationController
 
   def show
     @actor = Actor.find(params[:id])
-    debugger #remove later
+    #debugger #remove later
   end
 
   def new
@@ -13,11 +13,32 @@ class ActorsController < ApplicationController
     @actor = Actor.new(actor_params)    # Not the final implementation!
     if @actor.save
       flash[:success] = "You added a new actor to the database!"
-      redirect_to @actor
+      redirect_to action: "index"
     else
       render 'new'
     end
   end
+
+  def delete
+    @actor = Actor.find_by(params[:id])
+    actor.destroy
+    redirect_to action: "index"
+  end
+
+  def edit
+    @actor = Actor.find_by(params[:id])
+  end
+
+  def update
+    @actor = Actor.find(params[:id])
+
+    if @actor.update(actor_params)
+      redirect_to action: 'index'
+    else
+      render 'edit'
+    end
+  end
+
 
   def index
     @actors = Actor.all
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,9 +13,23 @@ class MoviesController < ApplicationController
     @movie = Movie.new(movie_params)    # Not the final implementation!
     if @movie.save
       flash[:success] = "You added a new movie to the database!"
-      redirect_to @movie
+      redirect_to action: "index"
     else
       render 'new'
+    end
+  end
+
+  def edit
+    @movie = Movie.find_by(params[:id])
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+
+    if @movie.update(movie_params)
+      redirect_to action: 'index'
+    else
+      render 'edit'
     end
   end
 
