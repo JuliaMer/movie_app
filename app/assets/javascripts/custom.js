52abe35cf183958ac0174848e7fd53889b2be010
Julia Merk <julia.merk@makandra.de>
2018-05-25 14:42:28 +0200

    added js file for movie new form validation

--- app/assets/javascripts/custom.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function () {
-    $(".cast-information").hide();
-})
---
+++ app/assets/javascripts/movie_new.js
@@ -0,0 +1,162 @@
+function validate_presence(id, input) {
+    if (input == "") {
+        $(id).addClass("is-invalid");
+        $(id).removeClass("is-valid");
+        return false;
+    } else {
+        $(id).removeClass("is-invalid");
+        $(id).addClass("is-valid");
+        return true;
+    }
+};
+
+function validate_length(id, input, num) {
+    if (input.length != num) {
+        $(id).addClass("is-invalid");
+        $(id).removeClass("is-valid");
+        return false;
+    } else {
+        $(id).removeClass("is-invalid");
+        $(id).addClass("is-valid");
+        return true;
+    }
+};
+
+$(document).ready(function () {
+    if ($("#new-movie-form").length > 0) {
+
+        window.alert("Hello, you are about to create a new movie. At least title and year are required!");
+
+        $("#title").focusout(function () {
+            var formInput = document.getElementById("movie_title").value;
+            validate_presence("#title", formInput);
+            /*
+            if (formInput == "") {
+                $("#title").addClass("is-invalid");
+                $("#title").removeClass("is-valid");
+            } else {
+                $("#title").removeClass("is-invalid");
+                $("#title").addClass("is-valid");
+            }
+            */
+        });
+
+
+        $("#year").focusout(function () {
+            var formInput = document.getElementById("movie_year").value;
+            if (!(validate_presence("#year", formInput))) {
+                document.getElementById("year-error").innerHTML = "Year must be filled out";
+            } else {
+                if (!(validate_length("#year", formInput, 4))) {
+                    document.getElementById("movie_year").value = "";
+                    document.getElementById("year-error").innerHTML = "Year must be four digits";
+                }
+            }
+
+            /*
+            if (formInput == "") {
+                $("#year").addClass("is-invalid");
+                $("#year").removeClass("is-valid");
+                document.getElementById("year-error").innerHTML = "Year must be filled out";
+            } else if (formInput.length != 4) {
+                $("#year").addClass("is-invalid");
+                $("#year").removeClass("is-valid");
+                document.getElementById("movie_year").value = "";
+                document.getElementById("year-error").innerHTML = "Year must be four digits";
+            } else {
+                $("#year").removeClass("is-invalid");
+                $("#year").addClass("is-valid");
+            }
+            */
+        });
+
+        $("#submit").click(function (event) {
+            if ((document.getElementById("movie_title").value) == "") {
+                event.preventDefault();
+                window.alert("Please insert title");
+
+            } else if ((document.getElementById("movie_year").value) == "" || (document.getElementById("movie_year").value.length != 4)) {
+                event.preventDefault();
+                window.alert("Please correct year");
+            }
+        });
+
+    }
+});
+
+//alternative ajax validation
+
+//$("#title").fadeOut(500).fadeIn(500);
+
+/*
+$("#title").ajaxSend(function(){
+   alert("Triggered ajaxSend handler.")
+});
+$( "#title" ).click(function() {
+   $( "#title" ).load( "movies/new.html" );
+});
+
+ */
+
+
+//old version (style with query css), try... catch...
+/*
+$(document).ready(function () {
+    window.alert("Hello, you are about to create a new movie. At least title and year are required!");
+    //window.alert(document.forms.length);
+    //window.alert(document.forms[0]);
+    //window.alert(document.forms);
+    //var formAction = form.attr("action");
+});
+
+$("#title").focusout(function () {
+    var formInput = document.getElementById("movie_title").value;
+    var redBorder = {
+        "border-style": "3px",
+        "border-radius": "5px", "border-color": "red"
+    };
+    var greenBorder = {
+        "border-style": "3px",
+        "border-radius": "5px", "border-color": "green"
+    };
+    try {
+        if (formInput == "") {
+            $(document.getElementById("movie_title")).css(redBorder);
+            throw "Title must be filled out";
+        } else {
+            $(document.getElementById("movie_title")).css(greenBorder);
+            document.getElementById("title-error").innerHTML = "";
+        }
+    } catch (err) {
+        document.getElementById("title-error").innerHTML = err;
+    }
+});
+
+$("#year").focusout(function () {
+    var formInput = document.getElementById("movie_year").value;
+    var redBorder = {
+        "border-style": "3px",
+        "border-radius": "5px", "border-color": "red"
+    };
+    var greenBorder = {
+        "border-style": "3px",
+        "border-radius": "5px", "border-color": "green"
+    };
+    try {
+        if (formInput == "") {
+            $(document.getElementById("movie_year")).css(redBorder);
+            throw "Year must be filled out";
+        } else if (formInput.length != 4) {
+            document.getElementById("movie_year").value = "";
+            $(document.getElementById("movie_year")).css(redBorder);
+            throw "Year must be four digits";
+        } else {
+            $(document.getElementById("movie_year")).css(greenBorder);
+            document.getElementById("year-error").innerHTML = "";
+        }
+    } catch (err) {
+        document.getElementById("year-error").innerHTML = err;
+    }
+});
+
+*/
